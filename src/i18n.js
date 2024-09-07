d36138fabefa31a763da7f4b17c796ad6a4e0c7d
joaoarthur <joaoabritto.dev@gmail.com>
2024-09-06 21:44:39 -0300

    build: Fixed some configs to deploy to github pages

--- src/i18n.js
+++ src/i18n.js
@@ -3,13 +3,17 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-i18n
-    .use(Backend) /** permite leitura dos arquivos no diretório public/locales ou de uma aplicação externa */
-    .use(LanguageDetector) /** detecta idioma do navegador */
-    .use(initReactI18next) /** inicializa a biblioteca */
-    .init({ /** opções de configuração da biblioteca */
-        fallbackLng: 'pt-BR' /** idioma utilizado caso o idioma detectado não seja suportado */,
-        supportedLngs: ['pt-BR', 'en-US'] /** idiomas suportados */,
-    });
+// Get the base URL from Vite
+const baseUrl = import.meta.env.BASE_URL || '/';
 
-export default i18n;
+i18n
+    .use(Backend) // Allows reading files from public/locales or external app
+    .use(LanguageDetector) // Detects browser language
+    .use(initReactI18next) // Initializes i18next
+    .init({
+        fallbackLng: 'pt-BR', // Fallback language
+        supportedLngs: ['pt-BR', 'en-US'], // Supported languages
+        backend: {
+            loadPath: `${baseUrl}locales/{{lng}}/{{ns}}.json` // Use the base URL for translation files
+        },
+    });
