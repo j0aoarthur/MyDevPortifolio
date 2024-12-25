ba85610c9f3f3fd8b9e69d56e612d602f69edf56
joaoarthur <joaoabritto.dev@gmail.com>
2024-12-25 05:27:12 -0300

    refactor: Improved the code in ProjectsSection

--- src/components/ProjectsSection/index.jsx
+++ src/components/ProjectsSection/index.jsx
@@ -4,42 +4,25 @@ import {ProjectComponent} from "../ProjectComponent/index.jsx";
 
 export function ProjectsSection() {
     const {t} = useTranslation();
+
+    const projects = t("projects", { returnObjects: true });
+
     return (
         <Section id={"projects"}>
             <h2>{t("project")}</h2>
             <ProjectsWrapper>
-                <ProjectComponent
-                    number={t("projects.01.number")}
-                    name={t("projects.01.name")}
-                    description={t("projects.01.description")}
-                    images={t("projects.01.images", {returnObjects: true})}
-                />
-                <ProjectComponent
-                    left
-                    number={t("projects.02.number")}
-                    name={t("projects.02.name")}
-                    description={t("projects.02.description")}
-                    images={t("projects.02.images", {returnObjects: true})}
-                    githubLink={t("projects.02.githubLink")}
-                    link={t("projects.02.link")}
-                />
-
-                <ProjectComponent
-                    number={t("projects.03.number")}
-                    name={t("projects.03.name")}
-                    description={t("projects.03.description")}
-                    images={t("projects.03.images", {returnObjects: true})}
-                    githubLink={t("projects.03.githubLink")}
-                />
-                <ProjectComponent
-                    left
-                    number={t("projects.04.number")}
-                    name={t("projects.04.name")}
-                    description={t("projects.04.description")}
-                    images={t("projects.04.images", {returnObjects: true})}
-                    githubLink={t("projects.04.githubLink")}
-                    link={t("projects.04.link")}
-                />
+                {Object.entries(projects).map(([key, project], index) => (
+                    <ProjectComponent
+                        key={key}
+                        left={index % 2 !== 0}
+                        number={project.number}
+                        name={project.name}
+                        description={project.description}
+                        images={project.images}
+                        githubLink={project.githubLink}
+                        link={project.link}
+                    />
+                ))}
             </ProjectsWrapper>
         </Section>
     )
